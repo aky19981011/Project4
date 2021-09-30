ff80a55637d8b0970d03b765a0fcd88fab0df0c8
aky19981011 <aky19981011@gmail.com>
2021-10-01 00:11:12 +0530

    first

---
+++ backend/data/notes.js
@@ -0,0 +1,16 @@
+const notes = [
+  {
+    _id: "1",
+    title: "Day 1 of college",
+    content: "your name",
+    category: "college",
+  },
+  {
+    _id: "2",
+    title: "Day2 of college",
+    content: "Learning ",
+    cotegory: "Learn",
+  },
+];
+
+export default notes;
